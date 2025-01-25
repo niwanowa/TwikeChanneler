7e51259604b66f50740d0a8e389c89fffbd54c1f
niwanowa <40478818+niwanowa@users.noreply.github.com>
2025-01-25 17:05:46 +0900

    ログ出力機能を追加: リクエストのURI、クエリパラメータ、リクエストボディをログに出力するように変更

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,7 +1,20 @@
 import json
+import logging
+import os
+import sys
 
 # import requests
 
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Add stdout handler
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stdout_handler.setFormatter(formatter)
+logger.addHandler(stdout_handler)
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -24,6 +37,33 @@ def lambda_handler(event, context):
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
+
+    logger.info("## EVENT")
+    logger.info(event)
+
+    # Log request URI
+    if "requestContext" in event and "http" in event["requestContext"]:
+        request_uri = event["requestContext"]["http"].get("path", "N/A")
+        logger.info(f"Request URI: {request_uri}")
+    else:
+        logger.info("Request URI: N/A")
+
+    # Log query parameters
+    if "queryStringParameters" in event and event["queryStringParameters"]:
+        query_params = event["queryStringParameters"]
+        logger.info(f"Query Parameters: {query_params}")
+    else:
+        logger.info("Query Parameters: N/A")
+
+    # Log request body
+    if "body" in event and event["body"]:
+        try:
+            request_body = json.loads(event["body"])
+            logger.info(f"Request Body: {request_body}")
+        except json.JSONDecodeError:
+            logger.info(f"Request Body: {event['body']}")
+    else:
+        logger.info("Request Body: N/A")
 
     # try:
     #     ip = requests.get("http://checkip.amazonaws.com/")
